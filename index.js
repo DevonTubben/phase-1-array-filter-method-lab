7e1450cb4b08107255cc3847242cd1778dd37eef
Devon Tubben <tubbend1@msu.edu>
2021-05-06 11:36:13 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(function(name){return name.toUpperCase() === string.toUpperCase()})
+  }
+  
+  function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.slice(0, string.length) === string)
+  }
+  
+  function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+  }
